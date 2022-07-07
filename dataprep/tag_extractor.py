5694c7ef6c2e42bcd325eb4f5918eb6e5ef2a202
Morshay <morshay23@gmail.com>
2022-07-07 04:07:36 +0300

    reworked tag extractor to yield less tags and images

--- dataprep/tag_extractor.py
+++ dataprep/tag_extractor.py
@@ -33,11 +33,19 @@ for file in Path('metadata').rglob('*'):
         for entry
         in metadata
         if entry['rating'] == 's'
-    })
+    }) # maybe content filtering in the future
 
     print('done!')
     
     del(metadata)
+
+# fn = 'danbooru_2020_safe_tags.json'
+
+# with open(fn, 'w') as f:
+#     json.dump(tags_dict)
+
+# with open(fn, 'r') as f:
+#     tags_dict = json.load(f)
 
 tags_df = pd.DataFrame(
     {
@@ -46,34 +54,29 @@ tags_df = pd.DataFrame(
     }
 )
 
-# deleting files that aren't in tags data
-
-img_paths = Path("images").rglob("./*.jpg")
-
-ids = [p.name.split('.')[0] for p in img_paths]
-
-ids_to_del = set(ids).difference(set(tags_df.id))
-
-files_to_del = [p for p in img_paths if p.name.split('.')[0] in ids_to_del]
-
-for file in files_to_del:
-    os.remove(file)
-
 # taking only the most used tags
 
 vc = pd.Series(
     [tag for tags in tags_dict.values() for tag in tags]
 ).value_counts()
 
-top_tags = pd.Series(vc[vc >= 1000].index.rename('tag')) # 3773 total
+top_tags = pd.Series(vc[vc >= 10000].index.rename('tag')) # 1002 total
+
+# vc>=100 gives ~10k tags
+# vc>=1000 gives 3773 tags
+# vc>=100000 gives 132 tags
 
 top_tags.to_csv('top_tags.csv', index=False)
 
 # filtering tag data 
 
+img_paths = Path("all-images").rglob("./*.jpg")
+
+ids = [p.stem for p in img_paths]
+
 tags_srs = tags_df[
     tags_df.id.isin(ids)
-].set_index('id').squeeze() # remains a string to fit filenames
+].set_index('id').squeeze()
 
 filtered_tags = tags_srs.apply(
     lambda tag_list: list(
@@ -81,4 +84,17 @@ filtered_tags = tags_srs.apply(
     )
 )
 
-filtered_tags.to_csv('img_tags.csv')
+# this is basically to ease on the nn
+
+tag_nums = filtered_tags.apply(len).sort_values()
+num_ids = tag_nums[(tag_nums<=20) & (tag_nums>=5)].index
+
+final_tags = tags_srs[tags_srs.index.isin(num_ids)]
+final_tags.to_csv('img_tags.csv')
+
+# deleting images out of tag data
+
+files_to_del = [p for p in img_paths if p.stem not in num_ids]
+
+for file in files_to_del:
+    os.remove(file)
